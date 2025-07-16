c79317c66575b057ca81b0c21d1572e736757332
Shad Azam <azamshad08@gmail.com>
2025-07-16 20:49:15 +0530

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-
-// const Home = ({ language }) => {
-//   const content = {
-//     en: {
-//       title: "Welcome to Chakzohra Masjid",
-//       desc: "A center for prayer, peace, and community unity.",
-//     },
-//     ur: {
-//       title: "چکزوہرا مسجد میں خوش آمدید",
-//       desc: "نماز، سکون اور کمیونٹی اتحاد کا مرکز۔",
-//     },
-//   };
-
-//   return (
-//     <div style={{ padding: "20px", direction: language === "ur" ? "rtl" : "ltr" }}>
-//       <h1>{content[language].title}</h1>
-//       <p>{content[language].desc}</p>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React from "react";
 import { Table } from "react-bootstrap";
 import timeTable from "../../assets/timeTableEnglish.json";
